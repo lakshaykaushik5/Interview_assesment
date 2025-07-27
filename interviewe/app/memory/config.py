ddde8b057b98e7b1a5bc0626956500666e269961
Lakshay Kaushik <lakshaykaushik051@gmail.com>
2025-07-27 10:54:11 +0530

    interviewee_init

---
+++ interviewe/app/memory/config.py
@@ -0,0 +1,78 @@
+from mem0 import Memory
+from dotenv import find_dotenv,load_dotenv
+import asyncio
+import os
+
+load_dotenv(find_dotenv())
+
+
+mem0_config = {
+    "vector_store":{
+        "provider":"qdrant",
+        "config":{
+            "url":os.getenv("QDRANT_URL"),
+            "collection_name":os.getenv("QDRANT_COLLECTION_NAME"),
+            "embedding_model_dims":1536
+        }
+    },
+    "graph_store":{
+        "provider":"neo4j",
+        "config":{
+            "url":os.getenv("NEO4J_URI"),
+            "username":os.getenv("NEO4J_USER"),
+            "password":os.getenv("NEO4J_PASSWORD")
+        }
+    },
+    "embedder":{
+        "provider":"openai",
+        "config":{
+            "model":os.getenv("EMBEDDING_MODEL"),
+            "api_key":os.getenv("EMBEDDING_API_KEY")
+        }
+    },
+    "llm":{
+        "provider":"openai",
+        "config":{
+            "model":os.getenv("MODEL_1_NAME"),
+            "api_key":os.getenv("MODEL_1_API_KEY")
+        }
+    }
+}
+
+
+mem0_conversation_config = {
+    "vector_store":{
+        "provider":"qdrant",
+        "config":{
+            "url":os.getenv("QDRANT_URL"),
+            "collection_name":os.getenv("QDRANT_COLLECTION_NAME"),
+            "embedding_model_dims":1536
+        }
+    },
+    "graph_store":{
+        "provider":"neo4j",
+        "config":{
+            "url":os.getenv("NEO4J_URI"),
+            "username":os.getenv("NEO4J_USER"),
+            "password":os.getenv("NEO4J_PASSWORD")
+        }
+    },
+    "embedder":{
+        "provider":"openai",
+        "config":{
+            "model":os.getenv("EMBEDDING_MODEL"),
+            "api_key":os.getenv("EMBEDDING_API_KEY")
+        }
+    },
+    "llm":{
+        "provider":"openai",
+        "config":{
+            "model":os.getenv("MODEL_1_NAME"),
+            "api_key":os.getenv("MODEL_1_API_KEY")
+        }
+    }
+}
+
+
+memory_conversation = Memory.from_config(mem0_conversation_config)
+memory_retrival = Memory.from_config(mem0_config)
---
+++ interviewe/app/memory/mem.py
@@ -0,0 +1,10 @@
+
+
+
+
+async def get_candidate_memories(candidate_id:str,limit:int=10):
+    try:
+        memories = ""
+        return memories
+    except Exception as e:
+        print(f"Error at get_candidate_memories :-: {e}")
